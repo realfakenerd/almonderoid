d5ccf3f89bb5b4e733370ddafb8a36aed578dbbf
realfakenerd <lucas210898@hotmail.com>
2022-05-24 02:10:42 -0300

    removed unnused file

--- src/lib/stores/sizes.ts
+++
@@ -1,9 +0,0 @@
-import {readable} from 'svelte/store';
-
-const canvasWidth = readable(window.innerWidth / 2);
-const canvasHeight = readable(window.innerHeight / 2);
-
-export {
-    canvasWidth,
-    canvasHeight
-}
