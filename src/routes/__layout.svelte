1955ed35d7963059d8b022772b7d0553f4c59562
realfakenerd <lucas210898@hotmail.com>
2022-06-07 00:30:45 -0300

    added play button to start game and better svgs

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,8 +3,17 @@
 	import '@fontsource/press-start-2p';
 </script>
 
+<header>
+	<nav class="navbar justify-center">
+		<div>
+			<a class="btn btn-link" href="/">game</a>
+			<a class="btn btn-link" href="/config">settings</a>
+		</div>
+	</nav>
+</header>
+
 <main class="grid place-items-center">
-	<div class="p-10">
+	<div class="px-10">
 		<slot />
 	</div>
 </main>
