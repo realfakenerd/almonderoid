c2b7db86a54b91fa0817a9bf0ef4c308cb022b9f
Roan Lopes <roan.lopes10@hotmail.com>
2023-10-15 03:22:28 -0300

    removendo varíaveis

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,20 +1,5 @@
-import type { Ship } from './objects';
-
-export const CANVAS_WIDTH = 640;
-export const CANVAS_HEIGHT = 480;
-
 export const random = (min: number, max: number) =>
 	((Math.random() * (max - min + 1)) & 0xffffffff) + min;
-/**
- * It resets the ship's position and velocity to the center of the screen and zero, respectively
- * @param {Ship} ship - The ship object that we want to reset.
- */
-export function resetShip(ship: Ship) {
-	ship.x = CANVAS_WIDTH / 2;
-	ship.y = CANVAS_HEIGHT / 2;
-	ship.velX = 0;
-	ship.velY = 0;
-}
 
 
 /* Creating an enum of the arrow keys. */
