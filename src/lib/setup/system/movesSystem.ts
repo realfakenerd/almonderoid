982ed1c1757673008ee03daa2166ba27b6989973
Roan Lopes <roan.lopes10@hotmail.com>
2023-10-21 23:37:03 -0300

    transformando em uma factory

--- src/lib/setup/system/movesSystem.ts
+++ src/lib/setup/system/movesSystem.ts
@@ -1,36 +1,38 @@
 import { stateGame } from "$lib/stores";
 import { get } from "svelte/store";
 
-let timer = 0;
 
-const moveObjects = () => {
-    const { ships, asteroids } = get(stateGame);
+export default function movesSystem() {
+    let timer = 0;
+    const moveObjects = () => {
+        const { ships, asteroids } = get(stateGame);
 
-    for (const ship of ships) {
-        ship.update();
+        for (const ship of ships) {
+            ship.update();
 
-        for (const bullet of ship.bullets) {
-            bullet.update();
+            for (const bullet of ship.bullets) {
+                bullet.update();
+            }
         }
+
+        for (const asteroid of asteroids) {
+            asteroid.update();
+        }
+    };
+
+    const startMoves = () => {
+        moveObjects();
+
+        timer = requestAnimationFrame(startMoves);
     }
 
-    for (const asteroid of asteroids) {
-        asteroid.update();
+    const stopMoves = () => {
+        cancelAnimationFrame(timer);
+        timer = 0;
+    };
+
+    return {
+        startMoves,
+        stopMoves
     }
-};
-
-const startMoves = () => {
-    moveObjects();
-
-    timer = requestAnimationFrame(startMoves);
-}
-
-const stopMoves = () => {
-    cancelAnimationFrame(timer);
-    timer = 0;
-};
-
-export {
-    startMoves,
-    stopMoves
 }
